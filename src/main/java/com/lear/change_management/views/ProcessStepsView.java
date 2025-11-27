dd54a3d86fd11aac125123c2fd02bc0fbcd25058
El Houadi <MElHouadi@lear.com>
2025-11-27 09:46:42 +0100

    project id retrieval routing to process step

--- src/main/java/com/lear/change_management/views/ProcessStepsView.java
+++ src/main/java/com/lear/change_management/views/ProcessStepsView.java
@@ -2,17 +2,33 @@ package com.lear.change_management.views;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.router.BeforeEvent;
-import com.vaadin.flow.router.HasUrlParameter;
-import com.vaadin.flow.router.Route;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Route(value = "/projects/processsteps/")
-public class ProcessStepsView extends Div implements HasUrlParameter<String> {
+public class ProcessStepsView extends VerticalLayout implements HasUrlParameter<String> {
 
     private Long projectId = 0L;
 
     @Override
-    public void setParameter(BeforeEvent beforeEvent, String s) {
-        this.projectId = Long.getLong(s);
+    public void setParameter(BeforeEvent beforeEvent, @OptionalParameter String s) {
+        Location location = beforeEvent.getLocation();
+        QueryParameters queryParameters = location.getQueryParameters();
+        Map<String, List<String>> parametersMap = queryParameters
+                .getParameters();
+        List<String> projectIdList = parametersMap.get(s);
+        String projectIdString = projectIdList.get(0);
+        try {
+            projectId = Long.valueOf(projectIdString);
+        } catch (NumberFormatException exception) {
+            projectId = 0L;
+        }
     }
+
+
+
 }
